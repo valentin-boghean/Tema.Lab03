65ac0613ad5d1fe7e4b15f8a728aec2c4f5983bd
valentin-boghean <boghy_junior@yahoo.com>
2020-03-20 19:54:54 +0200

    - Ex3

---
+++ EvidentaStudenti2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvidentaStudenti2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student("Valentin");
+            while(true)
+            {
+                foreach (string menu in s.Meniu())
+                {
+                    Console.WriteLine(menu);
+                }
+                Console.WriteLine();
+                char optiune;
+                if (args.Length != 0)
+                {
+                    optiune = 'C';
+                    //Console.Write("Linia de comanda nu contine argumente");
+                }
+                else
+                {
+                    Console.Write("Dati optiunea:");
+                    optiune = Console.ReadLine()[0];
+
+                }
+
+                switch (optiune)
+                {
+                    case 'S':
+
+                        string note;
+                        Console.WriteLine("Dati notele separate prin spatiu:");
+                        note = Console.ReadLine();
+
+                        //s.SetNote(note);
+                        break;
+                    case 'A':
+                        //Console.WriteLine(s.ConversieLaSir());
+                        s.ConversieLaSir();
+                        break;
+                    case 'T':
+                        Console.Write("Dati numarul de note:");
+                        int numar = Convert.ToInt32(Console.ReadLine());
+
+                        int[] noteINT = new int[numar];
+                        for (int i = 0; i < numar; i++)
+                        {
+                            Console.Write("Nota numarul {0}:", i);
+                            noteINT[i] = Convert.ToInt32(Console.ReadLine());
+                        }
+                        //s.SetNote(noteINT);
+                        break;
+                    case 'C':
+                        // s.SetNote(args);
+                        s.SetNote("1 2 3 4 5 , 4 5 6 7 8 , 3 4 5");
+                       
+                       // Console.WriteLine(s.ConversieLaSir());
+                        break;
+                    case 'X':
+                        Environment.Exit(0);
+                        break;
+                    case 'I':
+                        Console.WriteLine("Boghean Valentin, 3122A");
+                        break;
+                    default:
+                        Console.WriteLine("Optiune inexistenta.");
+                        break;
+                }
+                Console.ReadKey();
+                Console.Clear();
+                //s.SetNote("1 2 3 10 9 10 4 5 6");       //Test Ex 1: Metoda SetNote()
+                //s.SetNote("");
+            }
+        }
+    }
+}
---
+++ EvidentaStudenti2/Student.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvidentaStudenti2
+{
+    public class Student
+    {
+        // date membre private
+        string nume;
+        int[,] note;
+
+
+        // contructor implicit
+        public Student()
+        {
+        }
+
+        // constructor cu parametri
+        public Student(string _nume)
+        {
+            nume = _nume;
+        }
+
+        // Ex 1: Metoda SetNote utilizand split
+        public void SetNote(string sirNote)
+        {
+            note = new int[4,15];
+            //Eliminarea spatiilor dintre note 
+            string[] notes = new string[4];
+            notes = sirNote.Split(',');
+            string[] temp;
+            int i = 0;
+            foreach(string notari in notes)
+            {
+                temp = notari.Split(' ');
+               
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    Int32.TryParse(temp[j], out note[i, j]);
+                    //Array.Resize(note[i], note[i].Length);
+                    //Console.WriteLine(note[i, j]);
+                }
+                i++;
+                
+            }          
+        }
+        //Pentru citirea notelor de la tastatura pe rand
+        public void SetNote(int[,] _note)
+        {
+            note = new int[4,15];
+            note = _note;
+
+        }
+        public void ConversieLaSir()
+        {
+            string sNote = "Nu exista (Nu ati apelat metoda **setNote**)";
+            if (note != null)
+            {
+                sNote = string.Join(",", note);
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("Notele pe anul {0} sunt: ", i + 1);
+                for (int j = 0; j < 15; j++)
+                    Console.Write("{0}", note[i, j]);
+                Console.WriteLine();
+            }
+           // string s = string.Format("Studentul {0} are notele:.... {1}", nume, sNote);
+
+            //return s;
+        }
+        //Pentru citirea din linia de comanda
+       /* public void SetNote(string[] _note)
+        {
+            string noteF = String.Empty;
+            foreach (string note in _note)
+                noteF = noteF + note + " ";
+
+            SetNote(noteF);
+
+        }
+        */
+        //Ex 2: Meniu
+        public string[] Meniu()
+        {
+            string[] Meniu;
+            Meniu = new string[6];
+
+            Meniu[0] = "S: Citire note de la tastatura ca un sir de caractere";
+            Meniu[1] = "T: Citire note de la tastatura cate o nota pe rand";
+            Meniu[2] = "C: Citire note din linia de comanda";
+            Meniu[3] = "A: Afisare note";
+            Meniu[4] = "I: Info autor";
+            Meniu[5] = "X: Iesire";
+
+            return Meniu;
+
+        }
+        //Ex 3
+      
+    }
+}
